344daf339ca92800340a4a83fc10644ece43eeb6
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-10 16:11:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driver1,driver2){
+    return driver1.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === driver2.toLowerCase()
+
+    );
+
+}
+function fuzzyMatch(driver1,driver3){
+    return driver1.filter(
+    (possibleMatch) =>possibleMatch.toLowerCase().indexOf (driver3.toLowerCase()) ===0 
+    );
+}
+function matchName(driver1,driver2){
+return driver1.filter((record) => record.name === driver2);
+}
